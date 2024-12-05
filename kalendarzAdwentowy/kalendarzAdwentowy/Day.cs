95e725efb1238c35b6d90be6e68c21be0b6bb12e
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-12-05 13:45:30 +0100

    Add files via upload

---
+++ kalendarzAdwentowy/kalendarzAdwentowy/Day.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalendarzAdwentowy
+{
+    class Day
+    {
+        public int day;
+        public string content;
+        public string date;
+        public Day(int day, string content, string date)
+        {
+            this.day = day;
+            this.content = content;
+            this.date = date;
+        }
+    }
+}
---
+++ kalendarzAdwentowy/kalendarzAdwentowy/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kalendarzAdwentowy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Day> days = new List<Day>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            String timestamp = DateTimeOffset.Now.ToString("MMdd");
+            days.Add(new Day(1, "2 w cenie 3!", "1201"));
+            days.Add(new Day(2, "2 w cenie 3!", "1202"));
+            days.Add(new Day(3, "2 w cenie 3!", "1203"));
+            days.Add(new Day(4, "2 w cenie 3!", "1204"));
+        }
+        private void CreateCalendarUI()
+        {
+            var uniformGrid = new UniformGrid
+            {
+                Rows = 5,
+                Columns = 5
+            };
+            foreach(var day in days)
+            {
+                var button = new Button
+                {
+                    Content = day.day.ToString(),
+                    Tag = day,
+                    Background = System.Windows.Media.Brushes.LightGreen
+                };
+                button.Click += DayButton_Click;
+                uniformGrid.Children.Add(button);
+            }
+            MainGrid.Children.Add(uniformGrid);
+        }
+        private void DayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button && button.Tag is Day day)
+            {
+                MessageBox.Show($"Otworzyłeś: {day.day}!\nTreść: {day.content}", "Kalendaez Adwentowy");
+            }
+        }
+    }
+}
